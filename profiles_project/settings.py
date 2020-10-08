809f16ecbc88877ea06838952e49ff50a5b0a1de
alexandresillva <alexandresillva@gmail.com>
2020-10-07 21:31:14 -0300

    Added custom user profilr model, manager and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -3,18 +3,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '6zt6asr1rrr2-*0u_ir1ua@wx37@s@xc_(0$)r0_4k20-_9^oh'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
-
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
 
 # Application definition
 
